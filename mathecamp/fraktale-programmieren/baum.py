cd8dda53d21f093ceebeb74a0746b6bef161c8cf
Ingo Blechschmidt <iblech@web.de>
2015-03-19 20:25:56 +0100

    Dynamische Farben

--- mathecamp/fraktale-programmieren/baum.py
+++ mathecamp/fraktale-programmieren/baum.py
@@ -6,26 +6,33 @@
 from turtle import *
 
 speed(0)
+tracer(100,0)
 
 def tree(size):
     if size <= 10:
+        color((1500/size, 0, 0))
         forward(size)
         back(size)
     else:
+        color((1500/size, 0, 0))
         forward(size/3)
         left(30)
         tree(size*2/3)
         right(30)
+        color((1500/size, 0, 0))
         forward(size/6)
         right(25)
         tree(size/2)
         left(25)
+        color((1500/size, 0, 0))
         forward(size/3)
         right(25)
         tree(size/2)
         left(25)
+        color((1500/size, 0, 0))
         forward(size/6)
         back(size)
 
 left(90)
+back(150)
 tree(150)
