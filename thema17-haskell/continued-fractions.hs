71e5cfdab4fc12c78c153428e94c311c2828ee16
Ingo Blechschmidt <iblech@web.de>
2015-10-15 23:24:53 +0200

    Korrektes Verhalten bei rationalen Eingaben

--- thema17-haskell/continued-fractions.hs
+++ thema17-haskell/continued-fractions.hs
@@ -2,3 +2,6 @@ module Main where
 
 cf x = a : cf (1 / (x - fromIntegral a))
     where a = floor x
+
+cf' x = a : if x == fromIntegral a then [] else cf' (1 / (x - fromIntegral a))
+    where a = floor x
