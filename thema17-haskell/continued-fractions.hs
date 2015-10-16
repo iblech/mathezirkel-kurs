fa001c6c77f5044601d46189236bee85c19ef3c5
Ingo Blechschmidt <iblech@web.de>
2015-10-16 23:55:04 +0200

    Kommentare

--- thema17-haskell/continued-fractions.hs
+++ thema17-haskell/continued-fractions.hs
@@ -1,7 +1,11 @@
 module Main where
 
+-- Berechnet die Kettenbruchentwicklung einer Zahl, in der Annahme, dass alle
+-- auftretenden Reste verschieden von Null sind.
 cf x = a : cf (1 / (x - fromIntegral a))
     where a = floor x
 
+-- Berechnet die Kettenbruchentwicklung einer Zahl. Terminiert bei Auftreten
+-- eines verschwindenden Rests.
 cf' x = a : if x == fromIntegral a then [] else cf' (1 / (x - fromIntegral a))
     where a = floor x
