6bbdf3d8592825c835531f13b2dd9cf0e1b78c98
Ingo Blechschmidt <iblech@web.de>
2016-01-15 00:48:14 +0100

    Kompatibilität mit modernem GHCi

--- thema17-haskell/continued-fractions.hs
+++ thema17-haskell/continued-fractions.hs
@@ -11,4 +11,5 @@ cf' x = a : if x == fromIntegral a then [] else cf' (1 / (x - fromIntegral a))
     where a = floor x
 
 -- Evaluiert einen (endlichen) Kettenbruch.
+uncf :: (Fractional a) => [a] -> a
 uncf = foldr1 (\x y -> x + 1/y)
