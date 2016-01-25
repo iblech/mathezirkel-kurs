ba8dbd97e9d54d7f3fa82c7cecadd829963e67fe
Ingo Blechschmidt <iblech@web.de>
2016-01-25 23:39:28 +0100

    Spaß mit Z_10

--- thema17-haskell/p-adic.hs
+++ thema17-haskell/p-adic.hs
@@ -20,3 +20,8 @@ normalize (x:xs)
 zipWithDefault z (#) [] ys = map (z #) ys
 zipWithDefault z (#) xs [] = map (# z) xs
 zipWithDefault z (#) (x:xs) (y:ys) = (x # y) : zipWithDefault z (#) xs ys
+
+-- Zeuge, dass Z_10 kein Integritätsbereich ist:
+-- mapM_ (putStrLn . pad 100 . show) $ take 100 $ map (\i -> powermod (10^i) 5 (2^i)) [1..]
+-- let powermod m n i = if i == 0 then 1 else if i == 1 then n `mod` m else if i `mod` 2 == 0 then ((powermod m n (i `div` 2)) ^ 2) `mod` m else (n * powermod m n (i-1)) `mod` m
+-- let pad n xs = replicate (n - length xs) ' ' ++ xs
